650efd3eeadac247a64ef12e5772985477a46c47
Daniely <danixavier815@gmail.com>
2024-12-08 19:17:09 -0300

    Replicando exercicio de pesquisa Livros

---
+++ src/main/java/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,68 @@
+package main.java.list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livroList;
+
+    public CatalogoLivros(){
+        this.livroList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livroList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList){
+                if(l.getAutor().equalsIgnoreCase(autor)){
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+        List<Livro> livrosPorIntervalosAnos = new ArrayList<>();
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList){
+                if(l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal){
+                    livrosPorIntervalosAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervalosAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo = null;
+        if(!livroList.isEmpty()){
+            for(Livro l: livroList){
+                if(l.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+
+    public static void main(String[] args){
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 1800);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 1900);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 1920);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 2", 1940);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 3", 2021);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 3"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(1900, 1950));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+
+    }
+}
